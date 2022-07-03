18bc797eaccf362698c952916feb746d825bf258
heywinmeneseshp <77200722+heywinmeneseshp@users.noreply.github.com>
2022-07-02 23:24:27 -0500

    heroku

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const cors = require('cors');
 const routerApi = require('./routes');
 
 //Middlewares error
-const { logErrors, errorHandler, boomErrorHandler  } = require('./middlewares/error.handler')
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+/* Configuracion para limitar pediciones de otros dominios
+const whitelist = ['https://app-banarica.vercel.app'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+app.use(cors(corsOptions)); */
+app.use(cors()); //Acceso a todos los dominios
 
 app.get("/", (req, res) => {
   res.send("Hola, soy el servidor de la CI Banarica SA");
