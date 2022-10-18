3be14cc72ebc76e35f696f1c768893fe7d6e23bc
heywinmeneseshp <77200722+heywinmeneseshp@users.noreply.github.com>
2022-10-18 14:26:33 -0500

    Reparos en recepcion schema

--- schema/recepcion.schema.js
+++ schema/recepcion.schema.js
@@ -5,6 +5,8 @@ const remision = Joi.string().min(2);
 const observaciones = Joi.string().max(150);
 const cons_semana = Joi.string().min(5);
 const fecha = Joi.string();
+const aprobado_por = Joi.string();
+const realizado_por = Joi.string();
 
 const ingresarRemision = Joi.object({
   consecutivo: consecutivo,
@@ -12,6 +14,8 @@ const ingresarRemision = Joi.object({
   observaciones: observaciones.required(),
   cons_semana: cons_semana.required(),
   fecha: fecha.required(),
+  aprobado_por: aprobado_por,
+  realizado_por: realizado_por
 });
 
 const actualizarRemision = Joi.object({
@@ -20,6 +24,8 @@ const actualizarRemision = Joi.object({
   observaciones: observaciones,
   cons_semana: cons_semana,
   fecha: fecha,
+  aprobado_por: aprobado_por,
+  realizado_por: realizado_por
 });
 
 module.exports = { ingresarRemision, actualizarRemision };
