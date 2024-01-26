f7e566a722dcc4fb209ca131b53fcc73af911bda
heywinmeneseshp <77200722+heywinmeneseshp@users.noreply.github.com>
2024-01-25 23:43:01 -0500

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const env = {
-  apiKey: process.env.API_KEY || "2024",
-  secret: process.env.SECRET || "2024",
-  email: process.env.EMAIL || "meneses@craken.com.co",
-  password: process.env.PASSWORD || "0349",
+  apiKey: process.env.API_KEY,
+  secret: process.env.SECRET,
+  email: process.env.EMAIL,
+  password: process.env.PASSWORD,
 };
 
 module.exports = env;
