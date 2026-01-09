8489663be0302f4cfc3938fa6782db7c758c2a9e
heywinmeneseshp <77200722+heywinmeneseshp@users.noreply.github.com>
2026-01-09 09:00:33 -0500

    .

--- services/logistica/inspeccion.service.js
+++ services/logistica/inspeccion.service.js
@@ -43,6 +43,67 @@ class InspeccionService {
   }
 
   async paginate(offset, limit, filters = {}) {
+
+/*const busqueda = {
+    "cons_producto": [
+        "SEL5"
+    ],
+    "cons_almacen": [
+        "BAN",
+        "525",
+        "905",
+        "905",
+        "505",
+        "518",
+        "304",
+        "304",
+        "900",
+        "531",
+        "300",
+        "530",
+        "512",
+        "904",
+        "515",
+        "514",
+        "514",
+        "508",
+        "508",
+        "516",
+        "522",
+        "502",
+        "513",
+        "302",
+        "NAP",
+        "501",
+        "523",
+        "OBR",
+        "503",
+        "503",
+        "204",
+        "PORT",
+        "PRE",
+        "509",
+        "500",
+        "524",
+        "902",
+        "902",
+        "TRANS",
+        "517",
+        "YNP1",
+        "404",
+        "404",
+        "YNP2"
+    ],
+    "available": [
+        false
+    ],
+    "motivo_de_uso": "INSP02",
+    "contenedor": "rtr",
+    "fecha_inspeccion_inicio": "",
+    "fecha_inspeccion_fin": "2027-01-01"
+}*/
+
+
     const parsedOffset = (parseInt(offset) - 1) * parseInt(limit);
     const whereClause = { ...filters };
 
